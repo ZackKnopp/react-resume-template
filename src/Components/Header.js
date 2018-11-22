dc5e6f89845059f287e8f7d27099d0367eb61c98
Zack <zack.knopp@symplicity.ca>
2018-11-22 19:56:49 -0400

    Functional website, added in all my info, links, etc

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,7 +7,7 @@ class Header extends Component {
       var name = this.props.data.name;
       var description= this.props.data.description;
       var networks= this.props.data.social.map(function(network){
-        return <li key={network.name}><a href={network.url}><i className={network.className}></i></a></li>
+        return <li key={network.name}><a href={network.url} target="_blank"><i className={network.className}></i></a></li>
       })
     }
 
@@ -20,9 +20,9 @@ class Header extends Component {
               <li className="current"><a className="smoothscroll" href="#home">Home</a></li>
               <li><a className="smoothscroll" href="#about">About</a></li>
               <li><a className="smoothscroll" href="#resume">Resume</a></li>
-              <li><a className="smoothscroll" href="#portfolio">Works</a></li>
+              {/* <li><a className="smoothscroll" href="#portfolio">Works</a></li>
               <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-              <li><a className="smoothscroll" href="#contact">Contact</a></li>
+              <li><a className="smoothscroll" href="#contact">Contact</a></li> */}
           </ul>
         </nav>
         <div className="row banner">
